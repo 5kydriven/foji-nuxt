20b2ef08d584bfe64de582fe8220febec15721c4
5kydriven <tagpuno619@gmail.com>
2025-06-29 15:22:07 +0800

    feat: integrate motion-v library and enhance button styling in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
+	import { motion } from 'motion-v';
+
 	function onClick(e: any) {
 		console.log('Hello');
 	}
 </script>
 
 <template>
+	<motion.button
+		:whileHover="{ scale: 1.1 }"
+		:whilePress="{ scale: 0.95 }"
+		class="box"
+		@hoverStart="() => console.log('hover started!')"
+	/>
 	<UButton
 		label="click me"
 		icon="i-lucide-sun"
 		@click="onClick"
 	/>
 </template>
+
+<style>
+	.box {
+		width: 100px;
+		height: 100px;
+		background-color: #0d63f8;
+		border-radius: 10px;
+	}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxt/ui', '@nuxt/eslint'],
+	modules: ['@nuxt/ui', '@nuxt/eslint', 'motion-v/nuxt'],
 	pages: {
 		pattern: ['**/*.vue', '!**/components/**'],
 	},
